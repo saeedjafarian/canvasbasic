ef4b23818cd2af14bf881845cc70b915eda9ff62
saeedjafarian <saeedj808@gmail.com>
2021-07-15 19:36:18 +0430

    Change addeventListener

--- bundler/bundle.js
+++ bundler/bundle.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("const canvas = document.querySelector(\"#canvas\");\r\ncanvas.width = innerWidth;\r\ncanvas.height = innerHeight;\r\nconst context = canvas.getContext(\"2d\");\r\nconsole.log(getRandomArbitrary(5, 100));\r\n\r\n// resize\r\naddEventListener(\"resize\", function () {\r\n  canvas.width = innerWidth;\r\n  canvas.height = innerHeight;\r\n  context.fillStyle = \"#0C1C2A\";\r\n  context.fillRect(0, 0, canvas.width, canvas.height);\r\n});\r\n\r\n // Animate\r\n function animate() {\r\n    requestAnimationFrame(animate);\r\n    context.clearRect(0, 0, canvas.width, canvas.height);\r\n  }\r\n  animate();\n\n//# sourceURL=webpack://canvasbasic/./src/basic.js?");
+eval("const canvas = document.querySelector(\"#canvas\");\r\ncanvas.width = innerWidth;\r\ncanvas.height = innerHeight;\r\nconst context = canvas.getContext(\"2d\");\r\n\r\n// resize\r\naddEventListener(\"resize\", function () {\r\n  canvas.width = innerWidth;\r\n  canvas.height = innerHeight;\r\n});\r\n\r\n // Animate\r\n function animate() {\r\n    requestAnimationFrame(animate);\r\n    context.clearRect(0, 0, canvas.width, canvas.height);\r\n  }\r\n  animate();\n\n//# sourceURL=webpack://canvasbasic/./src/basic.js?");
 
 /***/ })
 
--- src/basic.js
+++ src/basic.js
@@ -2,14 +2,11 @@ const canvas = document.querySelector("#canvas");
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 const context = canvas.getContext("2d");
-console.log(getRandomArbitrary(5, 100));
 
 // resize
 addEventListener("resize", function () {
   canvas.width = innerWidth;
   canvas.height = innerHeight;
-  context.fillStyle = "#0C1C2A";
-  context.fillRect(0, 0, canvas.width, canvas.height);
 });
 
  // Animate
